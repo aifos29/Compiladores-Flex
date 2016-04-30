141f584a94da0bf228d44d40dca8b841c17d00e0
yasV <yasi1205@hotmail.com>
2016-04-29 21:03:24 -0600

    Cambio archivo microt.c
    Se identó, comentó, además se eliminaron los warnings que aparecian

--- microt.c
+++ microt.c
@@ -8,18 +8,18 @@ char token_buffer[200];
 FILE *archivo;
 FILE *salida;
 
-#define MAXIDLEN 33
-typedef char string[MAXIDLEN];
 
-extern void enter(string s);
-
+//Se definen los diferentes tipos a tratar
 typedef enum tipos_palabras{
-	INCLUDE,DEFINE,FIN,X //X ES IGUAL A CUALQUIER VALOR.
+	//ANYWORD ES IGUAL A CUALQUIER VALOR.
+	INCLUDE,DEFINE,FIN, ANYWORD 
 } palabras;
+
 
 //Variable para saber el temporal
 int cont=0;
 
+//Define el tamanno de los vectores nombre,valor
 int total=3;
 
 
@@ -28,39 +28,35 @@ void clear_buffer(void){
 	memset(token_buffer,'\0',strlen(token_buffer));
 }
 
-void buffer_char(int x){
+void buffer_char(int _val){
 	//Convierte el entero a caracter y lo agrega al token_buffer
 	if (strlen(token_buffer)==0){
-		char dato=(char)x;
-		char auxiliar[]={(char)x,'\0'};
+		char dato=(char)_val;
+		char auxiliar[]={(char)_val,'\0'};
 		strcat(token_buffer,auxiliar);
 	}
 	else{
-	char dato=(char)x;
-	 char auxiliar[]={dato,'\0'};
- 	 strcat(token_buffer,auxiliar);
+		char dato=(char)_val;
+		char auxiliar[]={dato,'\0'};
+		strcat(token_buffer,auxiliar);
 	}
-	
 }
 
 palabras reservado(){
 	//Revise el token_buffer y si este es una palabra reservada retorna el token al que pertenece
-
 	if (strcmp(token_buffer,"#define")==0){
-		return DEFINE;}
+		return DEFINE;
+	}
 	if (strcmp(token_buffer,"#include")==0){
-		return INCLUDE;}
-
+		return INCLUDE;
+	}
 	else{
-	 return X;
-  }
+		return ANYWORD;
+	}
 }
 
-
-
-palabras escaner(void)
-{
-	
+palabras escaner(void){
+	//Obtiene los tokens del texto
 	int in_char,c;
 
 	clear_buffer();
@@ -68,54 +64,54 @@ palabras escaner(void)
 		return FIN;
 
 	while (feof(archivo)==0){
+		//Leemos del archivo
 		in_char=fgetc(archivo);
+
+		//Si el token es espacio lo ignoro
 		if (isspace(in_char))
 			continue; /*do nothing */
-		else if (isalpha(in_char)||isgraph(in_char)){
 
+		//Si el token es un digito,minuscula,mayuscula o de puntuacion se agregan al buffer_char para imprimirlos
+		else if (isgraph(in_char)){
 			buffer_char(in_char);
-			for (c= fgetc(archivo);isalnum(c)||c=='='||c=='#';c=fgetc(archivo))
-			{	
-					buffer_char(c);}
-					return reservado();
-			
-
+			for (c= fgetc(archivo);isalnum(c);c=fgetc(archivo)){
+				buffer_char(c);
+			}
+			return reservado();
 		}else if (isdigit(in_char)){
 			buffer_char(in_char);
-			for (c=fgetc(archivo);isdigit(c)||isspace(c);c=fgetc(archivo))
+			for (c=fgetc(archivo);isdigit(c)||isspace(c);c=fgetc(archivo)){
 			 	if (isspace(c)){
-
 			 		return reservado();
-
-			}
+			 	}
 				else{
-
 					buffer_char(c);
 				}
+			}
+		}	
+	}
 }
-		
-}
-	
-}
-
 
 void asignar_nombre(char * nombre){
-    
+	//Almacena el nombre de la variable definida por el #define
     escaner();
 	strcpy(nombre,token_buffer);
 }
-void asignar_valor(char * valor){
-	escaner();
-	strcpy(valor,token_buffer);
 
-	
+void asignar_valor(char * valor){
+	//Almacena el valor de la variable definida por el #define
+	escaner();
+	strcpy(valor,token_buffer);	
 }
 
 
-void cambiador(char ** y,char **x){
+void cambiador(char **_nombre,char **_valor){
+	//Se encarga de leer el archivo de nuevo y asignar los valores almacenados segun el #define 
 	rewind(archivo);
+
 	palabras palabras = escaner();
-	int si=0;
+	int _estaDefinido=0;
+
 	while (palabras!=FIN){
 		switch(palabras){
 			case DEFINE:
@@ -126,32 +122,30 @@ void cambiador(char ** y,char **x){
 				printf("%s\n",token_buffer);
 				break;
 
-			case X:
-				si=0;
-				for (int i=0;i<cont;i++){
-					if (strcmp(token_buffer,y[i])==0){
-						printf("%s", x[i]);
-						si=1;
+			//Si es cualquier palabra busca alguna coincidencia con los valores definidos para sustituir
+			case ANYWORD:
+				_estaDefinido = 0;
+				int _indice;
+				for (_indice=0;_indice<cont;_indice++){
+					if (strcmp(token_buffer,_nombre[_indice])==0){
+						printf("%s", _valor[_indice]);
+						_estaDefinido = 1;
 						break;
 					}
 				}
-			
-				if (si==0){
+				//Si no esta definido solo imprimo
+				if (_estaDefinido == 0){
 					printf("%s",token_buffer);
 					break;
 				} 
-
-
 		}
 		palabras=escaner();
-
-
 	}
-
 }
 
 
 void reconocedor(){
+	//Se encarga de obtener los nombres y valores definidos por el #define
 	cont =0;
 	char guardar[33];
 	char guardar2[33];
@@ -165,54 +159,34 @@ void reconocedor(){
 	
 		switch(palabras){
 			case DEFINE:
-
-				asignar_nombre(&guardar);
+				//No se pasa con el & debido a que la estructura ya es *guardar
+				asignar_nombre(guardar);
 				nombre[cont]=strdup(guardar);
-				//free(guardar);
-				asignar_valor(&guardar2);
-
+				
+				asignar_valor(guardar2);
 				valor[cont]=strdup(guardar2);
-				cont ++;
 
-				
-				
+				cont ++;
 				break;
 			case INCLUDE:
-
-				
 				break;
 		
 			default:
 				break;
-
-		}
-
-
-		
-		palabras=escaner();
-
-
+		}	
+		palabras = escaner();
 	}
-		
-		cambiador(nombre,valor);
-		
-
+	
+	//Llamamos a cambiador para reescribir el texto por su respectivo valor	
+	cambiador(nombre,valor);
 }
-
-
-
 
 
 int main(int argc, char const *argv[])
 {
 	archivo=fopen("prueba.txt","r+");
 
-	
 	reconocedor();
-
-
-	
-	
 	
 	return 0;
 }
